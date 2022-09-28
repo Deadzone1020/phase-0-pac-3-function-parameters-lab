013f0cae689a1bb1705543e1fc4ab1926f328758
Jesus Ortiz <jesusestiff@gmail.com>
2022-09-27 20:43:17 -0600

    commit tree c9d2f6607de604018e24f4f5746db817ad4a4d11

---
+++ index.js
@@ -0,0 +1,20 @@
+function introduction(Bob){
+
+    return `Hi, my name is ${Bob}.`
+}
+  
+function introductionWithLanguage(Bob,language){
+
+    return `Hi, my name is ${Bob} and I am learning to program in ${language}.`
+} 
+
+function introductionWithLanguageOptional(name, language = "JavaScript"){
+
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+} 
+
+
+
+
+    
+  
